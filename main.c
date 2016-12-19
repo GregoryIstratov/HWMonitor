99815fd60f02f6a5023b0daefd3d2e2d2a95eca3
Greg Miller <gregory.istratov@gmail.com>
2016-12-19 19:59:30 +0300

    Header Licence

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/**************************************************************************************
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+                        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************************/
+
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <malloc.h>
@@ -263,7 +279,6 @@ enum {
 //======================================================================================================================
 // 64 bit
 // [16 bit reserved][16 bit value data][16 bit idx msg][8 bit depth][8 bit code]
-
 
 #define RET_MULTITHREADED
 
